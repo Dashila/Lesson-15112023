73ae6be90c3256afd6e7c37e4f03ea037097c4f9
Dasha <prokopevadara21@gmail.com>
2023-11-15 19:14:17 +0300

    мой первый комит

---
+++ Test1.py
@@ -0,0 +1,14 @@
+input requests
+import cmath
+
+
+q1 = input('Столица России?')
+print(q1)
+q2 = input('Столица Чебоксар?')
+print(q2)
+q3 = input('Как зовут человека рядом с тобой?')
+print(q3)
+q4 = input('сколько тебе лет?')
+print(q4)
+q5 = input('Столица англии?')
+print(q5)
